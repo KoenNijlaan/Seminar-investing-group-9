d489effd5f9d32fef2f5b8988af9052e473407c5
QuintenvanAmerongen <662165qa@eur.nl>
2026-03-11 14:05:13 +0100

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 #ewa drerries wat een kenker bende is dit now weer#
 print("Hello, World!")
 print("werkt het?")
+print("Hello, World!")
+print("werkt het?")
+print("Hello, World!")
+print("werkt het?")
